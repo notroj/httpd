9d45eb97fd2bb01b21c3225bbea3a98f87229b8e
Joe Orton <jorton@redhat.com>
2019-11-08 12:05:01 +0000

    Travis for 2.4, test v2.

---
+++ test/travis_before_linux.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -ex
+if ! test -v SKIP_TESTING; then
+   svn export -q https://svn.apache.org/repos/asf/httpd/test/framework/trunk test/perl-framework
+fi
+if test -v APR_VERSION; then
+    trunk_url=https://svn.apache.org/repos/asf/apr/apr/trunk
+    # If there is an APR trunk install cached in ~/root/apr-trunk,
+    # blow it away and rebuild it if the revision it was built from
+    # has changed.
+    if test $APR_VERSION = trunk; then
+        trunk_rev=`svn info --show-item last-changed-revision ${trunk_url}`
+        test -f $HOME/root/apr-trunk/.revision-is-${trunk_rev} || rm -rf $HOME/root/apr-trunk*
+    fi
+    if ! test -d $HOME/root/apr-${APR_VERSION}; then
+        case $APR_VERSION in
+            trunk) evn export -r ${trunk_rev} ${trunk_url} $HOME/build/apr-trunk ;;
+            *) svn export -q https://svn.apache.org/repos/asf/apr/apr/tags/${APR_VERSION} \
+                   $HOME/build/apr-${APR_VERSION} ;;
+        esac
+        pushd $HOME/build/apr-${APR_VERSION}
+        if [ $APR_VERSION = 1.4.1 ]; then
+            # 1.4.1 doesn't build with current libtool
+            svn cat https://svn.apache.org/repos/asf/apr/apr/tags/1.5.1/buildconf > buildconf
+        fi
+        ./buildconf
+        ./configure ${APR_CONFIG} --prefix=$HOME/root/apr-${APR_VERSION}
+        make -j2
+        make install
+        popd
+        APU_CONFIG="$APU_CONFIG --with-apr=$HOME/root/apr-${APR_VERSION}"
+    fi
+fi
+if test -v APU_VERSION; then
+    if ! test -d $HOME/root/apu-${APU_VERSION}; then
+        case $APU_VERSION in
+            trunk) url=https://svn.apache.org/repos/asf/apr/apr-util/trunk ;;
+            *) url=https://svn.apache.org/repos/asf/apr/apr-util/tags/${APU_VERSION} ;;
+        esac
+        svn export -q ${url} $HOME/build/apu-${APU_VERSION}
+        pushd $HOME/build/apu-${APU_VERSION}
+        ./buildconf --with-apr=$HOME/build/apr-${APR_VERSION}
+        ./configure ${APU_CONFIG} --prefix=$HOME/root/apu-${APU_VERSION}
+        make -j2
+        make install
+        popd
+    fi
+fi
