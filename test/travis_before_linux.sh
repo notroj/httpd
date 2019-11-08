d06b43382b6e2c73cf809fa2f7d7f9a3abd3692e
Joe Orton <jorton@redhat.com>
2019-11-08 13:29:56 +0000

    Add APR trunk build.

--- test/travis_before_linux.sh
+++ test/travis_before_linux.sh
@@ -3,12 +3,21 @@ if ! test -v SKIP_TESTING; then
    svn export -q https://svn.apache.org/repos/asf/httpd/test/framework/trunk test/perl-framework
 fi
 if test -v APR_VERSION; then
+    trunk_url=https://svn.apache.org/repos/asf/apr/apr/trunk
+    # If there is an APR trunk install cached in ~/root/apr-trunk,
+    # blow it away and rebuild it if the revision it was built from
+    # has changed.
+    if test $APR_VERSION = trunk; then
+        trunk_rev=`svn info --show-item last-changed-revision ${trunk_url}`
+        test -f $HOME/root/apr-trunk/.revision-is-${trunk_rev} || rm -rf $HOME/root/apr-trunk*
+    fi
     if ! test -d $HOME/root/apr-${APR_VERSION}; then
         case $APR_VERSION in
-            trunk) url=https://svn.apache.org/repos/asf/apr/apr/trunk ;;
-            *) url=https://svn.apache.org/repos/asf/apr/apr/tags/${APR_VERSION} ;;
+            trunk) svn export -r ${trunk_rev} ${trunk_url} $HOME/build/apr-trunk
+                   touch $HOME/root/apr-trunk/.revision-is-${trunk_rev} ;;
+            *) svn export -q https://svn.apache.org/repos/asf/apr/apr/tags/${APR_VERSION} \
+                   $HOME/build/apr-${APR_VERSION} ;;
         esac
-        svn export -q ${url} $HOME/build/apr-${APR_VERSION}
         pushd $HOME/build/apr-${APR_VERSION}
         if [ $APR_VERSION = 1.4.1 ]; then
             # 1.4.1 doesn't build with current libtool
