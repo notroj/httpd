19b61eda9644e5cfd98b1808b815aa24a0899602
Joe Orton <jorton@redhat.com>
2019-11-08 14:23:31 +0000

    Update comments.

--- test/travis_before_linux.sh
+++ test/travis_before_linux.sh
@@ -3,31 +3,31 @@ if ! test -v SKIP_TESTING; then
    svn export -q https://svn.apache.org/repos/asf/httpd/test/framework/trunk test/perl-framework
 fi
 if test -v APR_VERSION; then
+    # For APR trunk the cached version at ~/root/apr-trunk will be
+    # stale if the current trunk revision is different from that of
+    # the cached build.  Here, cache and check the rev number of the
+    # build accordingly.
     trunk_url=https://svn.apache.org/repos/asf/apr/apr/trunk
-    # If there is an APR trunk install cached in ~/root/apr-trunk,
-    # blow it away and rebuild it if the revision it was built from
-    # has changed.
     if test $APR_VERSION = trunk; then
         trunk_rev=`svn info --show-item last-changed-revision ${trunk_url}`
-        test -f $HOME/root/apr-trunk/.revision-is-${trunk_rev} || rm -rf $HOME/root/apr-trunk*
+        # Blow away the cached trunk install if the revision does not
+        # match.
+        test -f $HOME/root/apr-trunk/.revision-is-${trunk_rev} || rm -rf $HOME/root/apr-trunk
     fi
     if ! test -d $HOME/root/apr-${APR_VERSION}; then
         case $APR_VERSION in
-            trunk) svn export -r ${trunk_rev} ${trunk_url} $HOME/build/apr-trunk ;;
+            trunk) svn export -q -r ${trunk_rev} ${trunk_url} $HOME/build/apr-trunk ;;
             *) svn export -q https://svn.apache.org/repos/asf/apr/apr/tags/${APR_VERSION} \
                    $HOME/build/apr-${APR_VERSION} ;;
         esac
         pushd $HOME/build/apr-${APR_VERSION}
-        if [ $APR_VERSION = 1.4.1 ]; then
-            # 1.4.1 doesn't build with current libtool
-            svn cat https://svn.apache.org/repos/asf/apr/apr/tags/1.5.1/buildconf > buildconf
-        fi
         ./buildconf
         ./configure ${APR_CONFIG} --prefix=$HOME/root/apr-${APR_VERSION}
         make -j2
         make install
         if test -v trunk_rev; then
-            touch $HOME/root/apr-trunk/.revision-is-${trunk_rev}
+            # Record the revision built in the cache.
+            touch $HOME/root/apr-${APR_VERSION}/.revision-is-${trunk_rev}
         fi
         popd
         APU_CONFIG="$APU_CONFIG --with-apr=$HOME/root/apr-${APR_VERSION}"
